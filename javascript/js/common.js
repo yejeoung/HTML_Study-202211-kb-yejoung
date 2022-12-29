72f6dd77450c2ca56e46cb5b236207e5075e86d7
yejeoung <cyg99080312@gmail.com>
2022-12-29 12:47:47 +0900

    자바스크립트

---
+++ javascript/js/common.js
@@ -0,0 +1,12 @@
+const helloworld = () => {
+    alert('HellowWorld');
+}
+
+const moveExample02 = () => {
+    location.replace('./example02.html');
+    // location.href('./example02.html');
+}
+
+const back = () => {
+    history.back();
+}
